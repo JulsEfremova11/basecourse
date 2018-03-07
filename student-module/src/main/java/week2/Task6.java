209360334a292e757b5d8a41ad4aa89ed84c394b
JulsEfremova11 <36794113+JulsEfremova11@users.noreply.github.com>
2018-03-07 18:36:56 +0200

    Update Task6.java

--- student-module/src/main/java/week2/Task6.java
+++ student-module/src/main/java/week2/Task6.java
@@ -19,24 +19,30 @@ public class Task6 {
 
         int[] array = TaskUtils.arrayConverter(args);
 // int[] array = {84, 99, 43, 31, 12, 79, 6, 17, 55};     
+
+	  int[] array = {7, 2, 15, 99, 3, 14, 7};
+ 
 	  int sum1 = 0;     
 	  int sum2 = 0;               
-	  int arrayHalf = array.length / 2;             
-	for (int i=0; i < array.length; i++) {      
-		
+	  int arrayHalf = array.length / 2;      
+ 
+	 if (array.length % 2 == 0) {
+ 
+	 	for (int i=0; i < array.length; i++) {      
+ 
 	  	if (i >= 0 && i < arrayHalf) {    
-	  		
+ 
 			sum1 += array[i];  }      
-		
+ 
 	}
 	  	sum1= sum1/arrayHalf;   
-	  	
+ 
   	for (int i=arrayHalf; i < array.length; i++) {   
-  		
+ 
 		if (i >= arrayHalf && i < array.length) { 
-			
+ 
 			sum2 += array[i];  }      
-  		
+ 
   	} 
 		sum2 = sum2/arrayHalf;              
 System.out.print("{");
@@ -47,15 +53,84 @@ for (int i=0; i < arrayHalf-1; i++)  {
 	}  
 }
 	System.out.print(array[arrayHalf-1] + "}");
-} else {
+} else if (sum1 < sum2) {
 for(int i=arrayHalf; i < array.length-1; i++) {
+    System.out.print(array[i]+", ");
+ 
+} 
+ 
+	 System.out.print(array[array.length - 1] + "}");
+} 	else {
+	for (int i=0; i < array.length; i++)  {
+		 System.out.print(array[i]);
+	}
+}
+ 
+	 } 
+	 // проверка если длина массава непарная
+	 else
+ 
+	 	for (int i=0; i < array.length; i++) {      
+ 
+	  	if (i >= 0 && i < arrayHalf) {    
+ 
+			sum1 += array[i];  }      
+ 
+	}
+	  	sum1= sum1/arrayHalf;   
+ 
+  	for (int i=arrayHalf-1; i < array.length; i++) {   
+ 
+		if (i > arrayHalf && i < array.length) { 
+ 
+			sum2 += array[i];  }      
+ 
+  	} 
+		sum2 = sum2/arrayHalf;              
+System.out.print("{");
+if (sum1 > sum2) {
+for (int i=0; i < arrayHalf-1; i++)  {
+	if (sum1 > sum2) { 
+		System.out.print(array[i]+", "); 
+	}  
+}
+	System.out.print(array[arrayHalf-1] + "}");
+} 
+else if (sum1 < sum2) {
+for(int i=array.length-arrayHalf; i < array.length-1; i++) {
+    System.out.print(array[i]+", ");
+ 
+} 
+ 
+	 System.out.print(array[array.length - 1] + "}");
+} 	else {
+	for (int i=0; i < array.length; i++)  {
+		 System.out.print(array[i]);
+	}
+} else if (sum1 < sum2) {
+for(int i=arrayHalf; i < array.length-1; i++) {
+    System.out.print(array[i]+", ");
+ 
+} 
+ 
+	 System.out.print(array[array.length - 1] + "}");
+} 	else {
+	for (int i=0; i < array.length; i++)  {
+		 System.out.print(array[i]);
+	}
+}
+ 
+/*else {
+for(int i=array.length-arrayHalf; i < array.length-1; i++) {
     System.out.print(array[i]+", ");
     
 } 
-	
+ 
 	 System.out.print(array[array.length - 1] + "}");
-}
-        
+} */
+
+ 
+ 
 
     }
 }
