39a84694b51b4f1987893683b16c8e8367160a01
JulsEfremova11 <tzipa@ukr.net>
2018-03-11 01:46:21 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	student-module/src/main/java/week2/Task10.java
    #	student-module/src/main/java/week2/Task11.java
    #	student-module/src/main/java/week2/Task12.java
    #	student-module/src/main/java/week2/Task14.java
    #	student-module/src/main/java/week2/Task15.java
    #	student-module/src/main/java/week2/Task2.java
    #	student-module/src/main/java/week2/Task4.java
    #	student-module/src/main/java/week2/Task5.java
    #	student-module/src/main/java/week2/Task6.java
    #	student-module/src/main/java/week2/Task7.java
    #	student-module/src/main/java/week2/Task8.java
    #	student-module/src/main/java/week2/Task9.java
    #	student-module/src/test/java/week2/Task14Test.java

--- student-module/src/main/java/week2/Task10.java
+++ student-module/src/main/java/week2/Task10.java
@@ -6,15 +6,19 @@ import base.TaskUtils;
  * 10. Перевести положительно число с двоичной системы счисления в десятичную.
  * Число будет переданно в виде массива (int[] number).
  * И вывести на экран.
- *
+ * <p>
  * 1000 -> 8
  * 1111 -> 15
  */
 public class Task10 {
     public static void main(String[] args) {
-
         int[] number = TaskUtils.arrayConverter(args);
 
+        int res = 0;
 
+        for (int i = 0; i < number.length; i++) {
+            res += number[i] * (int) Math.pow(2, (number.length - 1 - i));
+        }
+        System.out.println(res);
     }
 }
